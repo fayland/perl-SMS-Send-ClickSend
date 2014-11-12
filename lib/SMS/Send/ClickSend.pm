6924072f4fca5beab1619b3cf31f4f0aa08a28c1
Fayland Lam <fayland@gmail.com>
2014-11-12 08:47:11 +0800

    initial commit

---
+++ lib/SMS/Send/ClickSend.pm
@@ -0,0 +1,39 @@
+package SMS::Send::ClickSend;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+SMS::Send::ClickSend - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use SMS::Send::ClickSend;
+
+=head1 DESCRIPTION
+
+SMS::Send::ClickSend is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
